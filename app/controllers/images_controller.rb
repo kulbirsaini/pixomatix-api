628d56cf60f259d56ce6da4dbddba28e65dd85f3
Kulbir Saini <saini@saini.co.in>
2015-05-12 20:07:24 +0530

    Add response headers for caching and accept byte range

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -16,6 +16,29 @@ class ImagesController < ApplicationController
   def original
     @image = Image.find(params[:id])
     image_path = @image.original_path || get_asset_path("default_gallery_image_original.png")
+
+    file_begin = 0
+    file_size = File.size(image_path)
+    file_end = file_size - 1
+
+    if !request.headers["Range"]
+      status_code = "200 OK"
+    else
+      status_code = "206 Partial Content"
+      match = request.headers['range'].match(/bytes=(\d+)-(\d*)/)
+      if match
+        file_begin = match[1]
+        file_end = match[1] if match[2] && !match[2].empty?
+      end
+      response.header["Content-Range"] = "bytes " + file_begin.to_s + "-" + file_end.to_s + "/" + file_size.to_s
+    end
+    response.header["Content-Length"] = (file_end.to_i - file_begin.to_i + 1).to_s
+    response.header["Last-Modified"] = File.mtime(image_path).to_s
+
+    response.header["Cache-Control"] = "public, must-revalidate, max-age=0"
+    response.header["Pragma"] = "no-cache"
+    response.header["Accept-Ranges"]=  "bytes"
+    response.header["Content-Transfer-Encoding"] = "binary"
     send_file image_path, type: @image.try(:mime_type) || 'image/png', disposition: 'inline'
   end
 
