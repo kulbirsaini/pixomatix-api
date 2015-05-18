b51d93fb7539578c809bd157c55fe9949e829764
Kulbir Saini <saini@saini.co.in>
2015-05-18 21:25:51 +0530

    Fixed issue with url generation

--- app/helpers/api/v1/images_helper.rb
+++ app/helpers/api/v1/images_helper.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     module ImagesHelper
       def get_absolute_url_for(path)
+        return path if path =~ /^http:/ || path =~ /^https:/
         File.join(request.base_url, path)
       end
     end
